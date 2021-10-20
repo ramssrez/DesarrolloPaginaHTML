96a92af0158fa85c1e75f03234fe5feabe76e00c
Raul Ramirez Perez <ramssrez@gmail.com>
2021-10-20 16:51:34 -0500

    Integración del JS de la libreria

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-alert("Alert desde JS");
+//alert("Alert desde JS");
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+           items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+$(".menu-icon").click(function(){
+    $("header nav").slideToggle();
+});
